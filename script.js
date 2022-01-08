1540bbf973d89378110dec3f1d00f3f7cd5cf3f3
Mauricio André Peraltilla Cuadros <peraltilla93@hotmail.com>
2022-01-08 00:02:58 -0500

    Presentación final. Práctica

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("page loaded...");
+
+//eventos hover
+function mouseOver (event){
+    console.log("Evento mouseOver", event);
+    console.log(event.play());
+    event.play()
+}
+
+function mouseOut (event){
+    console.log("Evento mouseOut", event);
+    event.pause()
+}
